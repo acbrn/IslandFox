ca508b086e292019cbfe826f1e08cf9d21da1a33
Andrew_Coburn <coburn188@gmail.com>
2024-05-30 11:51:15 -0500

    chore: Add Fox model to index.js and package.json

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,7 @@
 import HomeInfo from "./HomeInfo";
 import Loader from "./Loader";
 import Navbar from "./Navbar";
+import Alert from "./Alert";
+import Footer from "./Footer";
 
-export { HomeInfo, Loader, Navbar };
+export { HomeInfo, Loader, Navbar, Alert, Footer };
--- src/models/index.js
+++ src/models/index.js
@@ -2,5 +2,6 @@ import Bird from "./Bird";
 import Island from "./Island";
 import Plane from "./Plane";
 import Sky from "./Sky";
+import Fox from "./Fox";
 
-export { Bird, Island, Plane, Sky };
+export { Bird, Island, Plane, Sky, Fox };
